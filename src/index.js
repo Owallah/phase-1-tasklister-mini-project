5c68ec14e58a4375e2cc6638e4f9ed6abf14bbb7
Johnson <mikesonowallah@gmail.com>
2023-03-26 18:53:22 +0300

    Completed challenge

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildTodo(e.target.new_task_description.value)
+    form.reset()
+  })
 });
+
+function buildTodo(task) {
+  let list = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  list.textContent = `${task} `
+  list.appendChild(btn)
+  document.querySelector('#tasks').appendChild(list)
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
